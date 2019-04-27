bda3cd6f88348920e77327c2b5fcb0646ce69919
djin-cn <273218159@qq.com>
2019-04-27 12:37:01 +0800

    IP获取方式优化

--- src/main/java/me/djin/dcore/util/HostUtils.java
+++ src/main/java/me/djin/dcore/util/HostUtils.java
@@ -9,12 +9,21 @@ import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.util.Enumeration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * @author djin 主机工具类，获取主机信息
  */
 public class HostUtils {
+	private static final Logger LOGGER = LoggerFactory.getLogger(HostUtils.class);
+	
 	/**
 	 * 获取局域网IP，如果局域网IP获取不到将采用InetAddress.getLocalHost()获取的IP
+	 * 
+	 * 局域网网段一般为192.168.*、172.16.*-172.31.*、10.*
+	 * 
+	 * 优先192.168网段，其次172网段，最后10网段
 	 * @return
 	 */
 	public static InetAddress getLanInetAddress() {
@@ -43,22 +52,10 @@ public class HostUtils {
 			Enumeration<InetAddress> addressList = networkInterface.getInetAddresses();
 			while (addressList.hasMoreElements()) {
 				InetAddress inetAddress = addressList.nextElement();
-				//loopback类型的地址，一般为本机地址127.0.0.1
-				if(inetAddress.isLoopbackAddress()) {
-					System.out.println("Loopback IP:"+inetAddress.getHostAddress());
-					continue;
-				}
-				//非siteLocal类型地址，
-				if(!inetAddress.isSiteLocalAddress()) {
-					System.out.println("NotSiteLocal:"+inetAddress.getHostAddress());
-					continue;
-				}
-				if(inetAddress.isLinkLocalAddress()) {
-					System.out.println("SiteIP:"+inetAddress.getHostAddress());
-				}
-				
-				System.out.println(String.format("SiteIP:%s; IsLinkLocal:%s; isAnyLocal:%s; isMCGlobal:%s; isMCLinkLocal:%s; isMCNodeLocal:%s; isMCOrgLocal:%s; isMCSiteLocal:%s; isMulticast:%s;"
+				LOGGER.info(String.format("IP:%s; isLoopbackAddress:%s; isSiteLocalAddress:%s; IsLinkLocal:%s; isAnyLocal:%s; isMCGlobal:%s; isMCLinkLocal:%s; isMCNodeLocal:%s; isMCOrgLocal:%s; isMCSiteLocal:%s; isMulticast:%s;"
 						, inetAddress.getHostAddress()
+						, inetAddress.isLoopbackAddress()
+						, inetAddress.isSiteLocalAddress()
 						, inetAddress.isLinkLocalAddress()
 						, inetAddress.isAnyLocalAddress()
 						, inetAddress.isMCGlobal()
@@ -68,11 +65,19 @@ public class HostUtils {
 						, inetAddress.isMCSiteLocal()
 						, inetAddress.isMulticastAddress()
 						));
-				addr = inetAddress;
-//				break;
-			}
-			if(addr != null) {
-//				break;
+				
+				//loopback类型的地址，一般为本机地址127.0.0.1
+				if(inetAddress.isLoopbackAddress()) {
+					System.out.println("Loopback IP:"+inetAddress.getHostAddress());
+					continue;
+				}
+				//非siteLocal类型地址，
+				if(!inetAddress.isSiteLocalAddress()) {
+					continue;
+				}
+				if(addr == null || inetAddress.getHostAddress().compareTo(addr.getHostAddress()) > 0) {
+					addr = inetAddress;					
+				}
 			}
 		}
 		if(addr != null) {
